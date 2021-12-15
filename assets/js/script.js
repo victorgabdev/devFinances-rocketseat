8607743f295d5fec0028a512a16b23ff60ac5d80
Victor Costa <victo091198@gmail.com>
2021-12-15 18:31:12 -0300

    Create class Modal and DevFinance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
-const Modal = {
-  open(){
-    // Abrir modal
-    // Adicionar a class active ao modal
+class Modal {
+  static open(){
     document.querySelector('.modal-overlay').classList.add('active');
-  },
-  close(){
-    // fechar o modal
-    // remover a class active do modal
+  }
+  static close(){
     document.querySelector('.modal-overlay').classList.remove('active');
   }
+}
+
+class DevFinance {
+  linkForModal = document.querySelector('a.button.new')
+  linkForCloseModal = document.querySelector('a.button.cancel')
   
-};
+  init() {
+    document.addEventListener('click', (event) => {
+      if(event.target == this.linkForModal) Modal.open();
+      if(event.target == this.linkForCloseModal) Modal.close();
+    });
+
+
+  }
+}
+
+const devFinance = new DevFinance();
+devFinance.init();
